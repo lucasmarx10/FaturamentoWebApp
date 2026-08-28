0fb4787ffe25d9b8563acd6bae946dabccf375bd
lucasmarx10 <141359366+lucasmarx10@users.noreply.github.com>
2026-08-28 10:41:26 -0300

    habilitado categoria no app

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service worker simples: cache para funcionar offline.
-const CACHE = "faturamento-v2";
+const CACHE = "faturamento-v3";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (e) => {
